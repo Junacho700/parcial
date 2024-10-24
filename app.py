881b69487da38b93514b562da9ad761fef4d3a88
Ubuntu <ubuntu@ip-172-31-9-201.ec2.internal>
2024-10-24 02:37:26 +0000

    Juanito pruebas

--- app.py
+++ app.py
@@ -1,107 +1,114 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS  
-import pymysql
+from flask import Flask, jsonify, request
+import boto3
+import mysql.connector
+from mysql.connector import Error
+from flask_cors import CORS
+import time
+from dotenv import load_dotenv
 import os
-import sqlite3
+
+# Cargar variables de entorno desde el archivo .env
+load_dotenv()
 
 app = Flask(__name__)
-CORS(app)
 
-# Detectar si estamos en modo de pruebas
-if os.getenv('FLASK_ENV') == 'testing':
-    # Usar SQLite durante las pruebas
-    db_config = {
-        'database': 'test.db'
-    }
-else:
-    # Configura los datos de conexión a la base de datos MySQL para producción
-    db_config = {
-        'host': '172.31.83.172',  # La IP de la máquina donde está MySQL
-        'user': 'user',           # El usuario que creaste para la base de datos
-        'password': '123456789',  # La contraseña del usuario
-        'database': 'bigdata',    # El nombre de la base de datos
-        'cursorclass': pymysql.cursors.DictCursor
-    }
+# Configura CORS
+CORS(app, resources={r"/": {"origins": ""}}, supports_credentials=True)
 
-# Ruta para recibir el registro de usuarios
-@app.route('/register', methods=['POST'])
-def register_user():
-    # Obtener los datos enviados desde el frontend
-    data = request.get_json()
-    
-    first_name = data.get('firstName')
-    last_name = data.get('lastName')
-    birth_date = data.get('birthDate')
-    password = data.get('password')  # Recuerda que en producción no deberías almacenar contraseñas en texto plano
+# Configuración de credenciales de AWS para Athena
+AWS_ACCESS_KEY = os.getenv('AWS_ACCESS_KEY')
+AWS_SECRET_KEY = os.getenv('AWS_SECRET_KEY')
+AWS_SESSION_TOKEN = os.getenv('AWS_SESSION_TOKEN')
+REGION_NAME = os.getenv('REGION_NAME')
 
-    # Verificar que los campos no estén vacíos
-    if not first_name or not last_name or not birth_date or not password:
-        return jsonify({'message': 'Todos los campos son requeridos'}), 400
+# Inicializar el cliente de Athena con credenciales explícitas
+athena_client = boto3.client(
+    'athena',
+    region_name=REGION_NAME,
+    aws_access_key_id=AWS_ACCESS_KEY,
+    aws_secret_access_key=AWS_SECRET_KEY,
+    aws_session_token=AWS_SESSION_TOKEN
+)
 
-    # Establecer la conexión con la base de datos
-    if os.getenv('FLASK_ENV') == 'testing':
-        connection = sqlite3.connect(db_config['database'])
-        cursor = connection.cursor()
-        cursor.execute('''CREATE TABLE IF NOT EXISTS usuarios (
-                            id INTEGER PRIMARY KEY,
-                            first_name TEXT NOT NULL,
-                            last_name TEXT NOT NULL,
-                            birth_date TEXT NOT NULL,
-                            password TEXT NOT NULL
-                         )''')
-    else:
-        connection = pymysql.connect(**db_config)
-        cursor = connection.cursor()
+# Configuración de Athena
+ATHENA_DATABASE = 's3-sakila'
+ATHENA_OUTPUT_LOCATION = 's3://s3-sakila/'  # Cambia a tu bucket de salida
+
+# Credenciales de la RDS de MySQL
+DB_HOST = os.getenv('DB_HOST')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_NAME = os.getenv('DB_NAME')
+
+@app.route('/get-movies/<int:id_customer>', methods=['GET'])
+def get_movies(id_customer):
+    # Consulta para obtener el nombre de la película desde fact_venta y film
+    query = f"""
+        SELECT fv.customer_id, fv.film_id, f.title, fv.rental_id, fv.store_id, fv.rental_date, fv.date_sales_id, fv.amount
+        FROM fact_venta fv
+        JOIN film f ON fv.film_id = f.film_id
+        WHERE fv.customer_id = {id_customer}
+    """
 
     try:
-        # Crear la consulta SQL para insertar el usuario
-        if os.getenv('FLASK_ENV') == 'testing':
-            sql = "INSERT INTO usuarios (first_name, last_name, birth_date, password) VALUES (?, ?, ?, ?)"
-            cursor.execute(sql, (first_name, last_name, birth_date, password))
-        else:
-            sql = "INSERT INTO usuarios (first_name, last_name, birth_date, password) VALUES (%s, %s, %s, %s)"
-            cursor.execute(sql, (first_name, last_name, birth_date, password))
+        # Ejecutar la consulta en Athena
+        response = athena_client.start_query_execution(
+            QueryString=query,
+            QueryExecutionContext={'Database': ATHENA_DATABASE},
+            ResultConfiguration={'OutputLocation': ATHENA_OUTPUT_LOCATION}
+        )
+        
+        # Obtener el ID de ejecución de la consulta
+        query_execution_id = response['QueryExecutionId']
 
-        connection.commit()  # Guardar los cambios
-        return jsonify({'message': 'Usuario registrado exitosamente'}), 201
+        # Esperar a que la consulta se complete
+        status = 'RUNNING'
+        while status in ['RUNNING', 'QUEUED']:
+            response = athena_client.get_query_execution(QueryExecutionId=query_execution_id)
+            status = response['QueryExecution']['Status']['State']
+            if status in ['SUCCEEDED', 'FAILED', 'CANCELLED']:
+                break
+            time.sleep(1)
+
+        if status == 'SUCCEEDED':
+            # Obtener los resultados de la consulta
+            results = athena_client.get_query_results(QueryExecutionId=query_execution_id)
+            ventas = []
+            for row in results['ResultSet']['Rows'][1:]:  # Ignorar la primera fila (cabecera)
+                customer_id = row['Data'][0]['VarCharValue']
+                film_id = row['Data'][1]['VarCharValue']
+                title = row['Data'][2]['VarCharValue']
+                rental_id = row['Data'][3]['VarCharValue']
+                store_id = row['Data'][4]['VarCharValue']
+                rental_date = row['Data'][5]['VarCharValue']
+                date_sales_id = row['Data'][6]['VarCharValue']
+                amount = row['Data'][7]['VarCharValue']
+                ventas.append({
+                    'customer_id': customer_id,
+                    'film_id': film_id,
+                    'title': title,
+                    'rental_id': rental_id,
+                    'store_id': store_id,
+                    'rental_date': rental_date,
+                    'date_sales_id': date_sales_id,
+                    'amount': amount
+                })
+
+            return jsonify({
+                "status": "success",
+                "data": ventas
+            })
+        else:
+            return jsonify({
+                "status": "error",
+                "message": "La consulta a Athena falló o fue cancelada"
+            })
 
     except Exception as e:
-        print(f"Error al registrar el usuario: {e}")
-        return jsonify({'message': 'Error al registrar el usuario'}), 500
-
-    finally:
-        cursor.close()
-        connection.close()
-
-# Ruta para obtener todos los usuarios registrados
-@app.route('/users', methods=['GET'])
-def get_users():
-    if os.getenv('FLASK_ENV') == 'testing':
-        connection = sqlite3.connect(db_config['database'])
-        cursor = connection.cursor()
-    else:
-        connection = pymysql.connect(**db_config)
-        cursor = connection.cursor()
-
-    try:
-        # Obtener todos los usuarios de la base de datos
-        if os.getenv('FLASK_ENV') == 'testing':
-            cursor.execute("SELECT first_name, last_name, birth_date FROM usuarios")
-            users = cursor.fetchall()
-        else:
-            sql = "SELECT first_name, last_name, birth_date FROM usuarios"
-            cursor.execute(sql)
-            users = cursor.fetchall()
-
-        return jsonify(users), 200
-
-    except Exception as e:
-        print(f"Error al obtener usuarios: {e}")
-        return jsonify({'message': 'Error al obtener usuarios'}), 500
-
-    finally:
-        cursor.close()
-        connection.close()
+        return jsonify({
+            "status": "error",
+            "message": f"Error al obtener datos de ventas: {e}"
+        })
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(port=5000, host='0.0.0.0', debug=True)
